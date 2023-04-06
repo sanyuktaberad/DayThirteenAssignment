4bedd600b0ddffd63b1e3e8145dda7412fbc2d2e
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-06 17:29:43 +0530

    Refactor: All the three to one Generic Method for finding the maximum is added.

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,31 +1,28 @@
 package com.bridgelabz;
 
 public class FindMaximum {
-	public static void Maximum() {
-		String value1 = new String("Apple");
-		String value2 = new String("Banana");
-		String value3 = new String("Peach");
-		
-		int max1 = value1.compareTo(value2);
-		int max2 = value2.compareTo(value3);
-		int max3 = value3.compareTo(value1);
-		
-		if (max1 > 0) {
-			System.out.println("The maximum value among the three strings is: " + value1);
+
+	public static <A extends Comparable<A>> A maximum(A x, A y, A z) {
+		A max = x;
+
+		if (y.compareTo(max) > 0) {
+			max = y;
 		}
-		else if (max2 > 0) {
-			System.out.println("The maximum value among the three strings is: " + value2);
+
+		if (z.compareTo(max) > 0) {
+			max = z;
 		}
-		else if (max3 > 0) {
-			System.out.println("The maximum value among the three strings is: " + value3);
-		}
-		else {
-			System.out.println("All values are same.");
-		}
-	}
-	
-	public static void main(String[] args) {
-		Maximum();
+
+		return max;
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Maximum number among the three numbers is: " + maximum(1, 7, 3));
+		System.out.println(" ");
+
+		System.out.println("Maximum value among the three float values is: "+ maximum(2.7,6.98,11.55));
+		System.out.println(" ");
+		
+		System.out.println("Maximum value among the three string values is: "+ maximum("Apple", "Banana", "Peach"));
+	}
 }
