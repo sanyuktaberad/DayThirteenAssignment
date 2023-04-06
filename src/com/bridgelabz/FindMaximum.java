922eea1f470156d4a689ab4a670812b32f6b4760
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-06 17:46:14 +0530

    Refactor: Code for extending the maximum method for taking more than three paramertes is added.

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
-	public static <A extends Comparable<A>> A maximum(A x, A y, A z) {
+	public static <A extends Comparable<A>> A maximum(A x, A y, A z, A p, A q, A r) {
 		A max = x;
 
 		if (y.compareTo(max) > 0) {
@@ -12,17 +12,27 @@ public class FindMaximum {
 		if (z.compareTo(max) > 0) {
 			max = z;
 		}
+		if (p.compareTo(max) > 0) {
+			max = p;
+		}
+		if (q.compareTo(max) > 0) {
+			max = q;
+		}
+		if (r.compareTo(max) > 0) {
+			max = r;
+		}
+		
 
 		return max;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Maximum number among the three numbers is: " + maximum(1, 7, 3));
+		System.out.println("Maximum number among the three numbers is: " + maximum(1, 7, 3, 7, 45, 67));
 		System.out.println(" ");
 
-		System.out.println("Maximum value among the three float values is: "+ maximum(2.7,6.98,11.55));
+		System.out.println("Maximum value among the three float values is: "+ maximum(2.7, 6.98, 11.55, 98.99, 56.50, 67.66));
 		System.out.println(" ");
 		
-		System.out.println("Maximum value among the three string values is: "+ maximum("Apple", "Banana", "Peach"));
+		System.out.println("Maximum value among the three string values is: "+ maximum("Apple", "Banana", "Peach", "Lychee", "Grapes", "Guava"));
 	}
 }
